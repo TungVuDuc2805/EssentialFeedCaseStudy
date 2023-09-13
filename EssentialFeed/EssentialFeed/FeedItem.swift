e0810055c714293ce049ad51e5bf346f90e8037f
Tung Vu <tungvuduc2805@gmail.com>
2023-09-13 23:07:10 +0700

    add `FeedItem` model

---
+++ EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Tung Vu Duc on 13/09/2023.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
