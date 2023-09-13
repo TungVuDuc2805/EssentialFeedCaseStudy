4834ce55e237234d783bc9474cc1cffbb47310e0
Tung Vu <tungvuduc2805@gmail.com>
2023-09-14 00:36:33 +0700

    Move `HTTPClient` to its own file

---
+++ EssentialFeed/EssentialFeed/HTTPClient.swift
@@ -0,0 +1,17 @@
+//
+//  HTTPClient.swift
+//  EssentialFeed
+//
+//  Created by Tung Vu Duc on 14/09/2023.
+//
+
+import Foundation
+
+public enum HTTPClientResult {
+    case success(HTTPURLResponse)
+    case failure(Error)
+}
+
+public protocol HTTPClient {
+    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void)
+}
