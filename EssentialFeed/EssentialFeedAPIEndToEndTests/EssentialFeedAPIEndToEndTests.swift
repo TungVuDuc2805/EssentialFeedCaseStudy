45ab69158ef6bec93dd476b07de0a56752faf46f
Tung Vu <tungvuduc2805@gmail.com>
2023-09-20 14:09:22 +0700

    ensure that `load` works in integration in end to end test

---
+++ EssentialFeed/EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -0,0 +1,96 @@
+//
+//  EssentialFeedAPIEndToEndTests.swift
+//  EssentialFeedAPIEndToEndTests
+//
+//  Created by Tung Vu Duc on 20/09/2023.
+//
+
+import XCTest
+import EssentialFeed
+
+final class EssentialFeedAPIEndToEndTests: XCTestCase {
+    
+    func test_endToEndTestServerGETFeedImagesResult_matchesFixedTestData() {
+        let session = URLSession(configuration: .ephemeral)
+        let client = URLSessionHTTPClient(session: session)
+        let sut = RemoteFeedLoader(url: feedTestServerURL, client: client)
+        let exp = expectation(description: "Wait for completion")
+        
+        sut.load { result in
+            switch result {
+            case .failure(let error):
+                XCTFail("Expected successfully but got \(error) instead")
+            case .success(let items):
+                XCTAssertEqual(items.count, 8, "Expected 8 images in the test account image feed")
+                XCTAssertEqual(items[0], self.expectedImage(at: 0))
+                XCTAssertEqual(items[1], self.expectedImage(at: 1))
+                XCTAssertEqual(items[2], self.expectedImage(at: 2))
+                XCTAssertEqual(items[3], self.expectedImage(at: 3))
+                XCTAssertEqual(items[4], self.expectedImage(at: 4))
+                XCTAssertEqual(items[5], self.expectedImage(at: 5))
+                XCTAssertEqual(items[6], self.expectedImage(at: 6))
+                XCTAssertEqual(items[7], self.expectedImage(at: 7))
+            }
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 5.0)
+    }
+    
+    // MARK: - Helpers
+    private var feedTestServerURL: URL {
+        return URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
+    }
+    
+    private func expectedImage(at index: Int) -> FeedItem {
+        return FeedItem(
+            id: id(at: index),
+            description: description(at: index),
+            location: location(at: index),
+            imageURL: imageURL(at: index))
+    }
+    
+    private func id(at index: Int) -> UUID {
+        return UUID(uuidString: [
+            "73A7F70C-75DA-4C2E-B5A3-EED40DC53AA6",
+            "BA298A85-6275-48D3-8315-9C8F7C1CD109",
+            "5A0D45B3-8E26-4385-8C5D-213E160A5E3C",
+            "FF0ECFE2-2879-403F-8DBE-A83B4010B340",
+            "DC97EF5E-2CC9-4905-A8AD-3C351C311001",
+            "557D87F1-25D3-4D77-82E9-364B2ED9CB30",
+            "A83284EF-C2DF-415D-AB73-2A9B8B04950B",
+            "F79BD7F8-063F-46E2-8147-A67635C3BB01"
+        ][index])!
+    }
+    
+    private func description(at index: Int) -> String? {
+        return [
+            "Description 1",
+            nil,
+            "Description 3",
+            nil,
+            "Description 5",
+            "Description 6",
+            "Description 7",
+            "Description 8"
+        ][index]
+    }
+    
+    private func location(at index: Int) -> String? {
+        return [
+            "Location 1",
+            "Location 2",
+            nil,
+            nil,
+            "Location 5",
+            "Location 6",
+            "Location 7",
+            "Location 8"
+        ][index]
+    }
+    
+    private func imageURL(at index: Int) -> URL {
+        return URL(string: "https://url-\(index+1).com")!
+    }
+
+}
