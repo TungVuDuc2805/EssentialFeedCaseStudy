71ee89a18f2d1f4ff371ec630ef6b3010307bb91
Tung Vu <tungvuduc2805@gmail.com>
2023-09-20 16:55:10 +0700

    `save` requests cache deletion

---
+++ EssentialFeed/EssentialFeedTests/CacheFeedUseCaseTests.swift
@@ -0,0 +1,60 @@
+//
+//  CacheFeedUseCaseTests.swift
+//  EssentialFeedTests
+//
+//  Created by Tung Vu Duc on 20/09/2023.
+//
+
+import XCTest
+import EssentialFeed
+
+class FeedStore {
+    var deleteCachedFeedCallCount = 0
+    
+    func insert(_ items: [FeedItem]) {
+        deleteCachedFeedCallCount += 1
+    }
+}
+
+class LocalFeedLoader {
+    private let store: FeedStore
+    
+    init(store: FeedStore) {
+        self.store = store
+    }
+    
+    func save(_ items: [FeedItem]) {
+        store.insert(items)
+    }
+}
+
+class CacheFeedUseCaseTests: XCTestCase {
+    
+    func test_doesNotDeleteCacheUponCreation() {
+        let store = FeedStore()
+        _ = LocalFeedLoader(store: store)
+        
+        XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
+    }
+    
+    func test_save_requestsCacheDeletion() {
+        let store = FeedStore()
+        let sut = LocalFeedLoader(store: store)
+        let items = [uniqueItem(), uniqueItem()]
+        
+        sut.save(items)
+
+        XCTAssertEqual(store.deleteCachedFeedCallCount, 1)
+    }
+    
+    // MARK: - Helpers
+    private func uniqueItem(
+        id: UUID = UUID(),
+        description: String? = "any description",
+        location: String? = "any location",
+        image: URL = URL(string: "any-url")!
+    ) -> FeedItem {
+        return FeedItem(id: id, description: description, location: location, imageURL: image)
+    }
+    
+}
