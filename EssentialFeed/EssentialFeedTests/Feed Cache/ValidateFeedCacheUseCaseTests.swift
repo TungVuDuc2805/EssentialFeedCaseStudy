6c308b29fe158d54429f9147579960e2e1cb75f8
Tung Vu <tungvuduc2805@gmail.com>
2023-09-21 14:03:09 +0700

    `validate` deletes cache on expired cache

--- EssentialFeed/EssentialFeedTests/Feed Cache/ValidateFeedCacheUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/ValidateFeedCacheUseCaseTests.swift
@@ -47,6 +47,32 @@ class ValidateFeedCacheUseCaseTests: XCTestCase {
         XCTAssertEqual(store.messages, [.retrieve])
     }
     
+    func test_validate_deletesCacheOnExpirationCache() {
+        let currentDate = Date()
+        let expirationDate = currentDate.toExpirationDate()
+        
+        let (sut, store) = makeSUT(currentDate: { currentDate })
+        let (_, locals) = uniqueItems([uniqueFeedImage(), uniqueFeedImage()])
+
+        sut.validateCache()
+        store.completeRetrievalSuccessfully(with: locals, timestamp: expirationDate)
+        
+        XCTAssertEqual(store.messages, [.retrieve, .deletion])
+    }
+    
+    func test_validate_deletesCacheOnExpiredCache() {
+        let currentDate = Date()
+        let expiredDate = currentDate.toExpirationDate().adding(second: -1)
+        
+        let (sut, store) = makeSUT(currentDate: { currentDate })
+        let (_, locals) = uniqueItems([uniqueFeedImage(), uniqueFeedImage()])
+
+        sut.validateCache()
+        store.completeRetrievalSuccessfully(with: locals, timestamp: expiredDate)
+        
+        XCTAssertEqual(store.messages, [.retrieve, .deletion])
+    }
+    
     // MARK: - Helpers
     private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #file, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
         let store = FeedStoreSpy()
