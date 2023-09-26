3e2c8526e363cf1875eb53eebe0f615c302fa639
Tung Vu <tungvuduc2805@gmail.com>
2023-09-26 14:31:58 +0700

    replace `FeedViewControllerTests.LoaderSpy` with with `FeedLoader` protocol

--- EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -6,10 +6,20 @@
 //
 
 import XCTest
+import EssentialFeed
 
-class FeedViewController {
-    init(loader: FeedViewControllerTests.LoaderSpy) {
+class FeedViewController: UIViewController {
+    private var loader: FeedLoader?
+    
+    convenience init(loader: FeedLoader) {
+        self.init()
+        self.loader = loader
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
+        loader?.load { _ in }
     }
     
 }
@@ -23,9 +33,22 @@ class FeedViewControllerTests: XCTestCase {
         XCTAssertEqual(loader.loadFeedCallCount, 0)
     }
     
+    func test_viewDidLoad_loadsFeed() {
+        let loader = LoaderSpy()
+        let sut = FeedViewController(loader: loader)
+        
+        sut.loadViewIfNeeded()
+        
+        XCTAssertEqual(loader.loadFeedCallCount, 1)
+    }
+    
     // MARK: - Helpers
-    class LoaderSpy {
+    class LoaderSpy: FeedLoader {
         private(set) var loadFeedCallCount: Int = 0
+        
+        func load(completion: @escaping (LoadFeedResult) -> Void) {
+            loadFeedCallCount += 1
+        }
         
     }
     
