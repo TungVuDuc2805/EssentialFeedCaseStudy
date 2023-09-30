aedd0b2dbb2514d32bc303e4373a2b21ac1cb5a5
Tung Vu <tungvuduc2805@gmail.com>
2023-09-30 11:58:31 +0700

    move `MainQueueDispatchingDecorator` to its own file

---
+++ EssentialFeed/EssentialFeediOS/Feed UI/Composers/MainQueueDispatchingDecorator.swift
@@ -0,0 +1,50 @@
+//
+//  MainQueueDispatchingDecorator.swift
+//  EssentialFeediOS
+//
+//  Created by Tung Vu Duc on 30/09/2023.
+//
+
+import Foundation
+import EssentialFeed
+
+final class MainQueueDispatchingDecorator<T> {
+    let decoratee: T
+    
+    init(decoratee: T) {
+        self.decoratee = decoratee
+    }
+    
+    func dispatch(action: @escaping () -> Void) {
+        if Thread.isMainThread {
+            action()
+        } else {
+            DispatchQueue.main.async {
+                action()
+            }
+        }
+    }
+}
+
+extension MainQueueDispatchingDecorator: FeedLoader where T == FeedLoader {
+    
+    func load(completion: @escaping (EssentialFeed.LoadFeedResult) -> Void) {
+        decoratee.load { [weak self] result in
+            self?.dispatch {
+                completion(result)
+            }
+        }
+    }
+}
+
+
+extension MainQueueDispatchingDecorator: ImageLoader where T == ImageLoader {
+    
+    func loadImageData(from url: URL, completion: @escaping (Result<Data, Error>) -> Void) -> Cancellable {
+        return decoratee.loadImageData(from: url) { [weak self] result in
+            self?.dispatch {
+                completion(result)
+            }
+        }
+    }
+}
