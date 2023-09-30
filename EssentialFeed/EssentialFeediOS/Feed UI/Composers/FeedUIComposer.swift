723af3c9aa2d83b3b234b2f03fd492c58293d205
Tung Vu <tungvuduc2805@gmail.com>
2023-09-30 11:19:30 +0700

    move feed view controller creation and configuration to a factory method

--- EssentialFeed/EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
@@ -13,17 +13,25 @@ public final class FeedUIComposer {
     private init() {}
     
     public static func composeUIWith(feedLoader: FeedLoader, imageLoader: ImageLoader) -> FeedViewController {
-        let bundle = Bundle(for: FeedViewController.self)
-        let storyboard = UIStoryboard(name: "Feed", bundle: bundle)
-        let feedController = storyboard.instantiateInitialViewController() as! FeedViewController
-        feedController.title = FeedPresenter.title
         let presentationAdapter = FeedLoaderPresentationAdapter(loader: feedLoader)
-        feedController.loadFeed = presentationAdapter.load
+        let feedController = FeedViewController.makeWith(loadFeed: presentationAdapter.load, title: FeedPresenter.title)
         
         presentationAdapter.presenter = FeedPresenter(
             feedView: FeedImagePresentationAdapter(controller: feedController, loader: imageLoader),
             loadingView: WeakRefProxy(feedController)
         )
+        
+        return feedController
+    }
+}
+
+extension FeedViewController {
+    static func makeWith(loadFeed: @escaping () -> Void, title: String) -> FeedViewController {
+        let bundle = Bundle(for: FeedViewController.self)
+        let storyboard = UIStoryboard(name: "Feed", bundle: bundle)
+        let feedController = storyboard.instantiateInitialViewController() as! FeedViewController
+        feedController.loadFeed = loadFeed
+        feedController.title = title
         
         return feedController
     }
