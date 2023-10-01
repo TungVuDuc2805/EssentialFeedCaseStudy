f4447106639dd9cbc251c86f068badc7ec33ebb2
Tung Vu <tungvuduc2805@gmail.com>
2023-10-01 22:29:35 +0700

    Move `HTTPClientSpy` to shared scope

--- EssentialFeed/EssentialFeedTests/Feed API/LoadImageDataFromRemoteUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/LoadImageDataFromRemoteUseCaseTests.swift
@@ -174,25 +174,4 @@ class LoadImageDataFromRemoteUseCaseTests: XCTestCase {
         XCTAssertEqual(capturedErrors, [error], file: file, line: line)
     }
     
-    private class HTTPClientSpy: HTTPClient {
-        var messages = [(url: URL, completion: (HTTPClientResult) -> Void)]()
-        var requestedURLs: [URL] {
-            messages.map { $0.url }
-        }
-        
-        func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
-            messages.append((url, completion))
-        }
-        
-        func completeWithError(at index: Int = 0) {
-            let error = NSError(domain: "test", code: 0)
-            messages[index].completion(.failure(error))
-        }
-        
-        func completeWith(data: Data = Data(), statusCode code: Int = 200, at index: Int = 0) {
-            let response = HTTPURLResponse(url: requestedURLs[index], statusCode: code, httpVersion: nil, headerFields: nil)!
-            messages[index].completion(.success((data, response)))
-        }
-    }
-    
 }
