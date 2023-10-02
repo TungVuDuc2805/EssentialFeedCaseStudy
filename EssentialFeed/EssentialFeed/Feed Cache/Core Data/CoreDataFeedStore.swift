a43c5adbb2bb090704906c54b7ff8719b7c7500a
Tung Vu <tungvuduc2805@gmail.com>
2023-10-02 23:30:58 +0700

    move helper methods to a file

--- EssentialFeed/EssentialFeed/Feed Cache/Core Data/CoreDataFeedStore.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/Core Data/CoreDataFeedStore.swift
@@ -81,36 +81,3 @@ public final class CoreDataFeedStore: FeedStore {
     }
     
 }
-
-extension NSManagedObjectModel {
-    static func load(from name: String, in bundle: Bundle) -> NSManagedObjectModel? {
-        return bundle
-            .url(forResource: name, withExtension: "momd")
-            .flatMap {
-                NSManagedObjectModel(contentsOf: $0)
-            }
-    }
-}
-
-extension NSPersistentContainer {
-    enum Error: Swift.Error {
-        case invalidManagedObjectURL
-        case failedToLoadContainer
-    }
-
-    static func load(from name: String, model: NSManagedObjectModel, url: URL) throws -> NSPersistentContainer {
-        let description = NSPersistentStoreDescription(url: url)
-        let container = NSPersistentContainer(name: name, managedObjectModel: model)
-        container.persistentStoreDescriptions = [description]
-        
-        var capturedError: Swift.Error?
-        container.loadPersistentStores { _, error in
-            capturedError = error
-        }
-        if capturedError != nil {
-            throw Error.failedToLoadContainer
-        }
-        
-        return container
-    }
-}
